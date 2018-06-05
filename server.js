a988932253a42aaa5c362438989f2f0d466b5d61
grantposell <grantposell@gmail.com>
2018-06-04 22:45:44 -0500

    Fixed Server file

--- server.js
+++ server.js
@@ -34,11 +34,19 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// If no matching route is found default to home
+app.get("/savedarticles", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/savedarticles.html"));
+});
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("http://www.nyt.com/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
@@ -89,8 +97,8 @@ app.get("/articles", function (req, res) {
 // Route for getting all Articles from the db
 app.get("/saved", function (req, res) {
     // Grab every document in the Articles collection
-    db.Saved.find({})
-        .populate(err)
+    db.Article.find({ saved: true })
+        .populate("notes")
         .then(function (dbSaved) {
 
             // If we were able to successfully find Articles, send them back to the client
@@ -108,7 +116,7 @@ app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("note")
+        .populate("notes")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
@@ -119,21 +127,27 @@ app.get("/articles/:id", function (req, res) {
         });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
+// Route for saving/updating an article note
+app.post("/savearticles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-        .then(function (dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id },
-                { new: true });
+    db.Article.findOneAndUpdate({ _id: req.params.id },
+        { saved: true })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
             console.log("updating /articles/:id sending json");
             res.json(dbArticle);
         })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+// Route for saving/updating an Article's associated Note
+app.post("/unsavearticle/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: false })
         .then(function (dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
+            console.log("updating /articles/:id saving and sending json");
             res.json(dbArticle);
         })
         .catch(function (err) {
@@ -142,6 +156,28 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
+// Route for saving/updating an Article's associated Note
+app.post("/savenote/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    // save the new note that gets posted to the Notes collection
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            console.log(dbNote._id);
+            console.log("updating article: " + req.params.id + " with note: " + dbNote._id);
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, { new: true });
+        })
+        .then(function (dbArticle) {
+            console.log("success");
+            // If the Article was updated successfully, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            console.log("an error occurred: " + err);
+            console.log(err);
+            // If an error occurs, send it back to the client
+            res.json(err);
+        });
+});
 // Start the server
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
